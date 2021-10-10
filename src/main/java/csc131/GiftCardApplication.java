4889112bc602506fc86de3921bb8bbb1fb703eb4
Madison <madisonthompsonld@gmail.com>
2021-10-09 23:56:18 -0700

    Initial Commit

---
+++ src/main/java/csc131/GiftCardApplication.java
@@ -0,0 +1,19 @@
+package csc131;
+import java.util.ArrayList;
+
+public class GiftCardApplication {
+	public static void main(String[] args) {
+		GiftCard gc1 = new GiftCard(25, 125.00);
+		GiftCard gc2 = new GiftCard(50, 150.50);
+		ArrayList<GiftCard> giftCards = new ArrayList<GiftCard>();
+		giftCards.add(gc1);
+		giftCards.add(gc2);
+		
+		int gcLength = giftCards.size();
+		for(int i = 0; i < gcLength; i++) {
+			GiftCard gc = giftCards.get(i);
+			System.out.println("GiftCard " + gc.getId() + " from store# " + gc.getIssuingStore() + " has a balance of " + gc.getBalance());
+		}
+	}
+
+}
