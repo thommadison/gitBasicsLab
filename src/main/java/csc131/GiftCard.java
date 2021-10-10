b889bac468e1801bae9aaf0819736ca4037c46dd
Madison <madisonthompsonld@gmail.com>
2021-10-10 00:08:18 -0700

    third commit

--- src/main/java/csc131/GiftCard.java
+++ src/main/java/csc131/GiftCard.java
@@ -63,7 +63,7 @@ public class GiftCard
         
         if (amount < 0.0)
         {
-            result = "Invalid Transaction";
+            result = "Invalid Transaction: Nothing to deduct";
         }
         else
         {
